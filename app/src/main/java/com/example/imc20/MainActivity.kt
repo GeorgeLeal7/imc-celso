dd0c8cdfe3bfdf9630f4eb07099478f6c6dcbf0c
GEORGE CORREIA LEAL NETO <21193368@CFP127ALUNO.LOCAL>
2021-09-13 16:44:25 -0300

    Aula 13-09 - Efetuando o calculo IMC

--- app/src/main/java/com/example/imc20/MainActivity.kt
+++ app/src/main/java/com/example/imc20/MainActivity.kt
@@ -4,19 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //O override é para executar o onCreate da classe MainActivity, e não da classe AppCompatActivity.
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //O super quer dizer acima, ou seja, esse onCreate é da classe AppCompatAcitivity.
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editPeso = findViewById<EditText>(R.id.edit_peso)
-        val editAltura = findViewById<EditText>(R.id.edit_altura)
-        val buttonCalcular = findViewById<Button>(R.id.button_calcular)
+        //Diferença do val pra var é que val cria constante, e o var cria variaveis.
+        //Criando as instancias das views para que possam ser manipuladas no Kotlin;
+        val editTextPeso = findViewById<EditText>(R.id.edit_peso)
+        val editTextAltura = findViewById<EditText>(R.id.edit_altura)
+        val buttonCalcular =findViewById<Button>(R.id.button_calcular)
+        val textViewResultado = findViewById<TextView>(R.id.text_view__resultado)
 
-        buttonCalcular.setOnClickListener{
-            Toast.makeText(this, editPeso.text.toString(), Toast.LENGTH_SHORT).show()
+        //Adicionar um ouvinte de click no botão
+        buttonCalcular.setOnClickListener {
+
+            if(editTextPeso.text.isEmpty()){
+                editTextPeso.error = "Peso é obrigatório!"
+            } else if (editTextAltura.text.isEmpty()){
+                editTextAltura.error = "Altura é obrigatório!"
+            }else{
+                
+                val peso = editTextPeso.text.toString().toInt()
+                val altura = editTextAltura.text.toString().toDouble()
+
+                val imc = peso/(altura * altura)
+
+                //%.1f  =  A % representa qualquer coisa, e 1f seria: 1Float(nesse caso uma casa decimal)
+                textViewResultado.text = String.format("%.1f", imc)
+            }
+
         }
+
     }
 }
